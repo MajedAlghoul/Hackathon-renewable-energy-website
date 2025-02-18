fd2e81f14c206ae75d30503ff52e52d13d4f7cd2
Majed Alghoul <majed.alghoul3@gmail.com>
2025-02-18 19:37:57 +0200

    added call to action component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "./components/navBar/NavBar.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./components/footer/Footer.jsx";
 import Dashboard from "./components/dashboard/Dashboard.jsx";
 import Home from "./components/home/Home.jsx";
 function App() {
+  // added bootstrap functionality
+  useEffect(() => {
+    if (typeof window !== "undefined") import("bootstrap");
+  }, []);
+
   return (
     <Router>
       <NavBar />
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,11 @@
+import CallToAction from "../callToAction/CallToAction";
 import styles from "./Home.module.css";
 
 function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <CallToAction></CallToAction>
     </div>
   );
 }
