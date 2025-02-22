243adedfcc80a49f4f3921c3b4a67a107afeacad
Majed Alghoul <majed.alghoul3@gmail.com>
2025-02-22 11:14:39 +0200

    fixed bottom callto action button

--- src/components/callToAction/CallToAction.jsx
+++ src/components/callToAction/CallToAction.jsx
@@ -1,15 +1,25 @@
 import styles from "./CallToAction.module.css";
-
+import { useNavigate } from "react-router-dom";
 function CallToAction() {
+  const navigate = useNavigate();
+
+  const navigateTo = (path) => {
+    navigate(path);
+  };
   return (
     <div className={styles["container"]}>
       <p className={styles["subheading"]}>CALL TO ACTION</p>
       <h1 className={styles["heading"]}>
-        We are trusted by over{" "}
-        <span className={styles["highlight"]}>100+</span> clients. <br />
+        We are trusted by over <span className={styles["highlight"]}>100+</span>{" "}
+        clients. <br />
         Join them now and invest in Renewable Energies.
       </h1>
-      <button className={styles["cta-button"]}>Explore the Dashboard</button>
+      <button
+        onClick={() => navigateTo("/dashboard")}
+        className={styles["cta-button"]}
+      >
+        Explore the Dashboard
+      </button>
     </div>
   );
 }
